1ab81945383e609185d520d5d7a4141c0e0f17ad
ChrisG <gutchlord@cox.net>
2021-01-26 12:00:28 -0700

    01262021120000

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 const postCSSPlugins = [
     require('postcss-import'),
+    require('postcss-mixins'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('autoprefixer')
@@ -13,14 +14,21 @@ module.exports = {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
     },
+    // WebPack-Dev_server install reqments 
     devServer: {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
         contentBase: path.join(__dirname, 'app'),
         hot: true,
-        port: 3000
+        port: 3000,
+        // this allow devices on/same Netwrk access webpack-dev-server
+        host: '0.0.0.0'
     },
+
     // comments
     mode: 'development',
-    // watch: true,
+    // watch: true, not needed use webpack-dev-server
     module: {
         rules: [
             {
