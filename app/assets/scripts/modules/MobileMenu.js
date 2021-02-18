f7cac195914c903b8312c6859dbc4bfedf861daa
ChrisG <gutchlord@cox.net>
2021-02-18 13:35:43 -0700

    '021820211335'

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,5 +1,6 @@
 class MobileMenu {
   constructor() {
+    /*   save the dom */
     this.menuIcon = document.querySelector(".site-header__menu-icon")
     this.menuContent = document.querySelector(".site-header__menu-content")
     this.siteHeader =  document.querySelector(".site-header")
@@ -13,6 +14,7 @@ class MobileMenu {
   toggleTheMenu() {
     this.menuContent.classList.toggle("site-header__menu-content--is-visible")
     this.siteHeader.classList.toggle("site-header--is-expanded")
+    this.menuIcon.classList.toggle("site-header__menu-icon--close-x");
   }
 }
 
